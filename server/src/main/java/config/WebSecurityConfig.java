9dc3c406f6241347afd6799621e98e8acd4cbdc2
kimzerovirus <68390715+kimzerovirus@users.noreply.github.com>
2022-05-12 14:17:49 +0900

    corsFilter 임포트 변경

--- server/src/main/java/config/WebSecurityConfig.java
+++ server/src/main/java/config/WebSecurityConfig.java
@@ -2,12 +2,12 @@ package config;
 
 import com.kzv.server.security.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Slf4j
